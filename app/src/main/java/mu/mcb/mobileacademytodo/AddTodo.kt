017b0a03ffc88cf821b886e613e1f475ddfee578
Nick Mckenzie <nicholas.mckenzie@mcb.mu>
2020-03-27 11:48:29 +0400

    Implement validation for Title field

--- app/src/main/java/mu/mcb/mobileacademytodo/AddTodo.kt
+++ app/src/main/java/mu/mcb/mobileacademytodo/AddTodo.kt
@@ -2,10 +2,11 @@ package mu.mcb.mobileacademytodo
 
 import android.app.Activity
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.todo_details.*
+import android.view.View
+import android.os.AsyncTask
 
 class AddTodoActivity : AppCompatActivity() {
 
@@ -17,17 +18,41 @@ class AddTodoActivity : AppCompatActivity() {
         }
     }
 
+
     private fun validateAndSave(){
+
+        var isValid = true;
+
+        progressBar.visibility = View.VISIBLE;
+        saveTodo.isEnabled = false;
 
         var title = txtTitle.text.toString();
         var notes = txtNotes.text.toString();
         var date = txtDate.text.toString();
 
-        var todo = Todo(title, date);
-        todo.notes = notes
+        if(title.isEmpty())
+        {
+            txtTitle.error = "Required";
+            isValid = false;
+        }
 
-        ServiceLocator.getTodoRepo().Save(todo)
-        setResult(Activity.RESULT_OK)
-        finish()
+
+        if(isValid){
+            var todo = Todo(title, date);
+            todo.notes = notes
+
+            ServiceLocator.getTodoRepo().Save(todo)
+            setResult(Activity.RESULT_OK)
+
+            //Simulate network call.
+            AsyncTask.execute {
+                Thread.sleep(2500)
+                finish()
+            }
+        }
+        else{
+            progressBar.visibility = View.INVISIBLE;
+            saveTodo.isEnabled = true;
+        }
     }
 }
