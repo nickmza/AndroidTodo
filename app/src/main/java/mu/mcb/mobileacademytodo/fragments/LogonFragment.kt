ca5fa35ae117c94b75ac95f893057c3204c4f27f
Nick Mckenzie <nicholas.mckenzie@mcb.mu>
2020-04-20 16:31:14 +0400

    Add Firebase Authentication

--- app/src/main/java/mu/mcb/mobileacademytodo/fragments/LogonFragment.kt
+++ app/src/main/java/mu/mcb/mobileacademytodo/fragments/LogonFragment.kt
@@ -1,17 +1,25 @@
 package mu.mcb.mobileacademytodo.fragments
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.logon.*
 import mu.mcb.mobileacademytodo.Interfaces.IModalDialog
 import mu.mcb.mobileacademytodo.R
+import mu.mcb.mobileacademytodo.ServiceLocator
 
 class LogonFragment : Fragment(){
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,6 +32,11 @@ class LogonFragment : Fragment(){
         super.onActivityCreated(savedInstanceState)
 
         loginButton.setOnClickListener {
+
+            val imm: InputMethodManager =
+                activity?.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view!!.windowToken, 0)
+
             var user = emailText.text.toString()
             var password = passwordText.text.toString()
 
@@ -40,8 +53,17 @@ class LogonFragment : Fragment(){
 
             if(valid)
             {
-                val action = LogonFragmentDirections.actionLogonFragmentToTodoListFragment()
-                findNavController(it).navigate(action)
+                auth = FirebaseAuth.getInstance()
+
+                auth.signInWithEmailAndPassword(user, password)
+                    .addOnCompleteListener{ logonResult ->
+                        if(logonResult.isSuccessful){
+                            val action = LogonFragmentDirections.actionLogonFragmentToTodoListFragment()
+                            findNavController(it).navigate(action)
+                        }else{
+                            ServiceLocator.notificationService.notify("Invalid email address or password.")
+                        }
+                    }
             }
         }
     }
